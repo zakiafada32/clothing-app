9a7c7eb9485cd57d67c66e9035ad47b76d72bfba
Zaki Afada <afadazaki32@gmail.com>
2020-07-23 22:02:55 +0700

    redux persist

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import * as serviceWorker from './serviceWorker';
 
@@ -9,13 +10,15 @@ import './index.css';
 
 import App from './App';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
